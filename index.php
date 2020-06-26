1848cba3dc45324c489d0a925ce546b432e7ecb5
yeemans <42499530+yeemans@users.noreply.github.com>
2020-06-25 20:55:45 -0700

    Create index.php
    index

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Index</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="yee.jpg">
+    <style>
+      @import url('https://fonts.googleapis.com/css?family=Helvetica+Neue');
+      @import url('https://fonts.googleapis.com/css?family=Lato');
+      #title {
+        font-family: "Helvetica Neue";
+        text-align: center;
+        font-size: 2.0em;
+        color: white;
+      }
+      body {
+          background-image: url("index_background.jpg");
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+          background-position: center;
+          background-size: 100% 100%;
+      }
+      .center {
+        margin-left:auto;
+        margin-right:auto;
+        margin-top:40px;
+      }
+      th {
+        color: white;
+        font-family: Lato;
+        font-size: 1.5em;
+        margin:2%;
+      }
+      td {
+        color: white;
+        font-size: 1.25em;
+        padding-bottom: 5%;
+        font-family: Lato;
+      }
+      a {
+        color: white;
+        padding-right: 2%;
+
+        width: 300px;
+      }
+      table {
+        margin: auto;
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="title">Index</h1>
+    <table class="center">
+      <tr>
+        <th scope="col">Assignment</th>
+        <th scope="col">Type</th>
+        <th scope="col">Date</th>
+      </tr>
+      <tr>
+          <td><a target="blank" href="intro.html">Introducing Myself</a></td>
+          <td>HW</td>
+          <td>6/23</td>
+      </tr>
+      <tr>
+          <td><a target="blank" href="intro.php">Combining pages</a></td>
+          <td>Exercise</td>
+          <td>6/23</td>
+      </tr>
+      <tr>
+          <td><a target="blank" href="michael_favorite_things.php">My Favorite things</a></td>
+          <td>Exercise</td>
+          <td>6/23</td>
+      </tr>
+      <tr>
+          <td><a target="blank" href="spoiler_alert.php">Hot Takes</a></td>
+          <td>HW</td>
+          <td>6/25</td>
+      </tr>
+      <tr>
+          <td><a target="blank" href="gettingtoknowyou.php">Getting to know you</a></td>
+          <td>Exercise</td>
+          <td>6/25</td>
+      </tr>
+      <tr>
+          <td><a target="blank" href="quiz.html">Quiz Project</a></td>
+          <td>Project</td>
+          <td></td>
+      </tr>
+  </body>
+
+</html>
